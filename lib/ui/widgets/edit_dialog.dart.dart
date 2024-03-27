58fce114cb7099fb1bb97dbf5295717c2c7fec45
Diego Antunes <diegocazetta@gmail.com>
2024-03-27 17:12:33 -0300

    Edit and save major number of fields

--- lib/ui/widgets/edit_dialog.dart.dart
+++ lib/ui/widgets/edit_dialog.dart.dart
@@ -24,23 +24,99 @@ class _EditDialogState extends State<EditDialog> {
     _editedPessoa = widget.pessoa;
   }
 
+  Widget _buildTextField(
+      String label, String value, Function(String) onChanged) {
+    return TextFormField(
+      initialValue: value,
+      onChanged: onChanged,
+      decoration: InputDecoration(labelText: label),
+    );
+  }
+
+  String bytesToHex(List<int> bytes) {
+    return bytes.map((byte) => byte.toRadixString(16).padLeft(2, '0')).join();
+  }
+
+  List<int> hexToBytes(String hex) {
+    var bytes = <int>[];
+    for (var i = 0; i < hex.length; i += 2) {
+      var byteString = hex.substring(i, i + 2);
+      var byteValue = int.parse(byteString, radix: 16);
+      bytes.add(byteValue);
+    }
+    return bytes;
+  }
+
+  Widget _buildByteField(
+      String label, List<int> byteValue, Function(List<int>) onChanged) {
+    return TextFormField(
+      initialValue: bytesToHex(byteValue),
+      onChanged: (val) => onChanged(hexToBytes(val)),
+      decoration: InputDecoration(labelText: label),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: const Text('Edit User'),
       content: SingleChildScrollView(
-        child: ListBody(
+        child: Column(
           children: <Widget>[
-            TextFormField(
-              initialValue: _editedPessoa.vbNome,
-              onChanged: (value) {
-                setState(() {
-                  _editedPessoa.vbNome = value;
-                });
-              },
-              decoration: InputDecoration(labelText: 'Name'),
+            _buildTextField('Parent ID', _editedPessoa.iIDPai.toString(),
+                (val) => _editedPessoa.iIDPai = int.parse(val)),
+            _buildTextField('Name', _editedPessoa.vbNome,
+                (val) => _editedPessoa.vbNome = val),
+            _buildTextField('Type', _editedPessoa.bTipo,
+                (val) => _editedPessoa.bTipo = val),
+            _buildTextField('Password', _editedPessoa.vbSenha,
+                (val) => _editedPessoa.vbSenha = val),
+            _buildTextField('RFID', _editedPessoa.vbRfid,
+                (val) => _editedPessoa.vbRfid = val),
+            _buildTextField(
+                'NFC', _editedPessoa.vbNfc, (val) => _editedPessoa.vbNfc = val),
+            _buildTextField(
+                'Pav', _editedPessoa.vbPav, (val) => _editedPessoa.vbPav = val),
+            _buildTextField('Weekday', _editedPessoa.bDiaSemana,
+                (val) => _editedPessoa.bDiaSemana = int.parse(val)),
+            _buildByteField('Initial Hour', _editedPessoa.vbHrInicial,
+                (val) => _editedPessoa.vbHrInicial = val),
+            _buildByteField('Final Hour', _editedPessoa.vbHrFinal,
+                (val) => _editedPessoa.vbHrFinal = val),
+            _buildTextField(
+                'Can Register',
+                _editedPessoa.bPodeCadastrar.toString(),
+                (val) => _editedPessoa.bPodeCadastrar = int.parse(val)),
+            _buildTextField(
+              'Can Release',
+              _editedPessoa.bPodeLiberar.toString(),
+              (val) => _editedPessoa.bPodeLiberar = int.parse(val),
             ),
-            // Add more TextFormFields for other fields of PessoaStruct
+            _buildTextField(
+              'Edited',
+              _editedPessoa.bEditado.toString(),
+              (val) => _editedPessoa.bEditado = int.parse(val),
+            ),
+            /*_buildByteField(
+              'Apartment',
+              _editedPessoa.bApartamento,
+              (val) => _editedPessoa.bApartamento = val,
+            ),
+            _buildTextField(
+              'Version',
+              _editedPessoa.vbVersao,
+              (val) => _editedPessoa.vbVersao = val,
+            ),
+            _buildByteField(
+              'Initial Date Time',
+              _editedPessoa.vbDataHoraInicial,
+              (val) => _editedPessoa.vbDataHoraInicial = val,
+            ),
+            _buildByteField(
+              'Final Date Time',
+              _editedPessoa.vbDataHoraFinal,
+              (val) => _editedPessoa.vbDataHoraFinal = val,
+            ),*/
           ],
         ),
       ),
